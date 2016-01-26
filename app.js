18c1c04494338ea72249d256e1c1db1073967aef
Christopher P.K Morris <christopher.pkm@gmail.com>
2016-01-26 23:42:05 +0000

    functional version up and running

--- app.js
+++ app.js
@@ -1,2 +1,21 @@
-/* TODO:get the state working first MVP
+/* TODO:get the state working first MVP, flow control
 then worry about the view */
+function bubbleSort(a){
+  var a;
+  var temp;
+  console.log(a);
+  for (var i = 0; i < a.length; i++) {
+    for (var j = 0; j < a.length; j++) {
+      if (a[j] > a[j + 1]) {
+         temp = a[j];
+         // swap
+         a[j] = a[j + 1];
+         a[j + 1] = temp;
+      }
+    }
+  }
+
+  console.log(a);
+}
+
+bubbleSort([7, 5, 4, 2, 1, 3, 6]); 
