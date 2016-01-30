4de3e0aba10ddec5cc26247c642de00cf51fa8c6
Christopher P.K Morris <christopher.pkm@gmail.com>
2016-01-30 15:38:40 +0000

    working on view code

--- app.js
+++ app.js
@@ -7,36 +7,43 @@ function bubble(a){
   var a,
       temp;
 
-
-
   return {
     // bubble sort through an array
     sort: function() {
-      for (var i = 0; i < a.length; i++) {
-        for (var j = 0; j < a.length; j++) {
-          if (a[j] > a[j + 1]) {
-             temp = a[j];
-             // swap(bubble)
-             a[j] = a[j + 1];
-             a[j + 1] = temp;
-          }
+
+      var i = 0;
+
+      var sorting = window.setInterval(function(){
+
+      i++;
+      // update visual every few millasecs.
+      visual.barChart(a);
+
+      for (var j = 0; j < a.length; j++) {
+        if (a[j] > a[j + 1]) {
+           temp = a[j];
+           // swap(bubble)
+           a[j] = a[j + 1];
+           a[j + 1] = temp;
         }
       }
 
-    return a;
+      console.log(a);
+      if(i === a.length) stopTimer(sorting);
+
+    }, 300 );
+
+    function stopTimer(timer){
+      clearInterval(timer);
+    }
+
+
+
     },
+
     // scramble an array
     scramble: function() {
       // keep scrambling untill all random numbers have been placed.
-      var index;
-      while(!randomNumbers){
-      // index1 = random number;
-      // loop through and check what numbers are left to scramble and narrow down.
-
-      }
-      Math.random(a.length);
-
-
     },
 
     plain: function(){
@@ -50,27 +57,49 @@ function bubble(a){
 var result = bubble([7, 5, 4, 2, 1, 3, 6, 10, 8, 9]);
 console.log(result.plain());
 console.log(result.sort());
-console.log(result.scramble());
 
 
 
-function visual(map, current){
-    map parsed array to a new array
-    var bars = [];
 
-    for (var i = 0; i < mapped.length; i++) {
-      bars.push(i*5);
-    }
 
-    parent.childNodes[current].style.height = bars[map[i]] + "px";
+// function visual(arr, current){
+    // map parsed array to a new array, do i need to map?
+    // var mapped = old.map(function());
+  //   var bars = [];
+  //
+  //   for (var i = 0; i < mapped.length; i++) {
+  //     bars.push(i*5);
+  //   }
+  //
+  //   parent.childNodes[current].style.height = bars[arr[i]] + "px";
+  // }
+
+
+
+
+
+var visual = function(){
+
+  var switch = true;
+
+  return {
+
+  barChart: function(arr, current){
+      var bars = [];
+      if(switch){
+        var parent = document.createElement("div");
+        document.appendChild(parent);
+      }
+      switch = false;
+
+      for (var i = 0; i < arr.length; i++) {
+        bars.push(arr[i] * 5);
+        var bar = document.createElement("div");
+        parent.appendChild(bar)
+      }
+
+
+      parent.childNodes[current].style.height = bars[arr[i]] + "px";
   }
-
-
-
 }
-
-function barChart(array){
-    create array.length of floated div elements.
-    or a canvas graph.
-
 }
